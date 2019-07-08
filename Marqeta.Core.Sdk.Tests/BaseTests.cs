52770550fa4f1834f5a120a9c744c646ce0f98c9
Liam Flanagan <liam.flanagan@capitalontap.com>
2019-07-08 11:04:43 +0100

    Added some integration tests, will need to orgnise these a bit more later.

---
+++ Marqeta.Core.Sdk.Tests/BaseTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Xunit.Abstractions;
+
+namespace Marqeta.Core.Sdk.Tests
+{
+    public abstract class BaseTests
+    {
+        protected readonly ITestOutputHelper TestOutputHelper;
+        protected static IConfigurationRoot Config = new ConfigurationHelper().GetConfiguration();
+
+        protected static string BaseUrl = Config["Marqeta:BaseUrl"];
+        protected static string UserName = Config["Marqeta:UserName"];
+        protected static string Password = Config["Marqeta:Password"];
+
+        public BaseTests(ITestOutputHelper testOutputHelper)
+        {
+            TestOutputHelper = testOutputHelper;
+        }
+
+        public static IMarqetaClient GetMarqetaClient()
+        {
+            return new MarqetaClient(BaseUrl, UserName, Password);
+        }
+    }
+}
