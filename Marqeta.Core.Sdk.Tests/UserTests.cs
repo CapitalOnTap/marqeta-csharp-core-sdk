c9b0d676ea64301b59f6b396b84eb3bb8e82c1e2
Liam Flanagan <liam.flanagan@capitalontap.com>
2019-08-07 08:38:40 +0100

    Split integration tests into more files / contexts

---
+++ Marqeta.Core.Sdk.Tests/UserTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace Marqeta.Core.Sdk.Tests
+{
+    public class UserTests : BaseTests
+    {
+        public UserTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+        }
+
+        [Fact]
+        public async void UsersGetAsync()
+        {
+            var client = GetMarqetaClient();
+            var response = await client.UsersGetAsync();
+            Assert.NotNull(response);
+            Assert.True(response.Count > 0);
+        }
+    }
+}
