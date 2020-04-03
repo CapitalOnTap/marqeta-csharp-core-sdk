b6bf5ea2021c77cf787e90aeb4b6f4265b14e09b
John Cyril <john.cyril@capitalontap.com>
2020-04-03 21:39:47 +0100

    reverted change to gpa order helper

--- Marqeta.Core.Sdk.Tests/Helpers/GpaOrderHelper.cs
+++ Marqeta.Core.Sdk.Tests/Helpers/GpaOrderHelper.cs
@@ -21,7 +21,7 @@ namespace Marqeta.Core.Sdk.Tests.Helpers
             {
                 User_token = userToken,
                 Amount = fundingAmount,
-                Currency_code = "GBP",
+                Currency_code = "USD",
                 Funding_source_token = fundingSourceToken
             };
             var gpaResponse = await client.GpaordersPostAsync(gpaRequest);
