75abe0719b462d709201c556b13ea961fc8afeff
Haydn Dias <7255658+HaydnDias@users.noreply.github.com>
2025-08-01 09:54:10 +0100

    Emergency fix to add a new enum value of UNSCHEDULED_CARD_ON_FILE to transaction_initiated_category

--- Marqeta.Core.Sdk/Generated/Models/Terminal_model_transaction_initiated_category.cs
+++ Marqeta.Core.Sdk/Generated/Models/Terminal_model_transaction_initiated_category.cs
@@ -59,5 +59,9 @@ namespace Marqeta.Core.Sdk.Models
         #pragma warning disable CS1591
         REAUTHORIZATION,
         #pragma warning restore CS1591
+        [EnumMember(Value = "UNSCHEDULED_CARD_ON_FILE")]
+        #pragma warning disable CS1591
+        UNSCHEDULED_CARD_ON_FILE,
+        #pragma warning restore CS1591
     }
 }
