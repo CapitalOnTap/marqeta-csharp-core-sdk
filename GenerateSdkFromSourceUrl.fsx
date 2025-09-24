81a170e72db6526329afc4ed518a7b0ed45d63c7
Patrick.Nkulikiyinka <patrick.nkulikiyinka@capitalontap.com>
2025-09-24 17:16:01 +0100

    add UNSCHEDULED_CARD_ON_FILE enum value to transaction_initiated_category

--- GenerateSdkFromSourceUrl.fsx
+++ GenerateSdkFromSourceUrl.fsx
@@ -233,6 +233,22 @@ module OpenApiHelpers =
         applyTransactionModelTypeModifications(openApiSchema)
 
     /// <summary>
+    ///     Applies modifications to the Point of sale schema.
+    /// </summary>
+    /// <param name="openApiSchema">The OpenApiSchema object representing pos.</param>
+    let private applyPosModelModifications (openApiSchema: OpenApiSchema) =
+        let propertyKey = "transaction_initiated_category"
+        let enumToAdd = "UNSCHEDULED_CARD_ON_FILE"
+
+        // If the transaction_initiated_category property enum exists, add the new value
+        if openApiSchema.Properties.ContainsKey(propertyKey) then
+            let propertyEnum = openApiSchema.Properties[propertyKey].Enum
+            let enumString = OpenApiString(enumToAdd) 
+
+            if not(propertyEnum.Contains(enumString)) then
+                propertyEnum.Add(enumString)
+
+    /// <summary>
     ///     Takes a schema and applies relevant modifications.
     /// </summary>
     /// <param name="openApiSchema">The OpenApiSchema to apply modifications to.</param>
@@ -309,6 +325,9 @@ module OpenApiHelpers =
             
         if openApiSchemas.ContainsKey("transaction_model") then
             applyTransactionModelModifications(openApiSchemas["transaction_model"])
+
+        if openApiSchemas.ContainsKey("pos") then
+            applyPosModelModifications(openApiSchemas["pos"])
         
     /// <summary>
     ///     Applies modifications to the Components on an OpenAPI spec.
